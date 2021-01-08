cf64ebe146bd4d116cc2cef262cd1b3e4d5619d5
enel <enel@gmail.com>
2021-01-08 12:11:15 +0000

    stage 1

---
+++ src/components/Main/Form/styles.js
@@ -0,0 +1,15 @@
+import { RadioGroup } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+export default makeStyles(() =>({
+    radioGroup:{
+        didpaly: 'flex',
+        justifyContent: 'center',
+        marginBottom:'-10px',
+
+    },
+    button:{
+        marginTop:'20px',
+    }
+}))
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { SpeechProvider } from '@speechly/react-client';
+
+import { Provider } from './context/context';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <SpeechProvider appId="cdfa93e9-9882-4f9a-99e0-eb8384417436" language="en-US">
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
+  document.getElementById('root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
